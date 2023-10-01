59a1ea25f34a2ec2c6eb48e13ac983bd2bbac4d2
ivanros <ivanros@protonmail.com>
2023-10-01 20:20:07 +0200

    footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        //1 rem = 4px
+      '13': '3.25rem',
+      '15': '3.75rem',
+      '112':'28rem',
+      '128': '32rem',
+      '144': '36rem',
+    },},
   },
   plugins: [],
 }
